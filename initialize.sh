45f025fa81cd2eeb919e6d98a4ecc423a1debd46
afeiship <1290657123@qq.com>
2018-04-16 16:03:37 +0800

    issue: install in centos

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
+
+
+## centos
+# https://blog.csdn.net/farmwang/article/details/78257810
+# npm: relocation error: npm: symbol SSL_set_cert_cb
+yum update openssl
+
+## install
+yum -y install nodejs

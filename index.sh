2a16162861401a6a00a40cf7e6ba07a7d53d5abf
afeiship <1290657123@qq.com>
2019-06-27 23:26:33 +0800

    fix: nodejs install

--- index.sh
+++ index.sh
@@ -8,4 +8,10 @@ source $ROOT_PATH/npm-afeiship.sh;
 
 export NODE_PATH=$HOME/.node_modules;
 
+# brew install nodejs
+export PATH="/usr/local/opt/openssl/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+
 unset ROOT_PATH;

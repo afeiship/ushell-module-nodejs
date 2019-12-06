f4b43f98275a2f42032005fdc3d1634cc7bf5133
afeiship <1290657123@qq.com>
2019-12-06 14:12:49 +0800

    fix: modify global path

--- index.sh
+++ index.sh
@@ -8,6 +8,9 @@ source $ROOT_PATH/npm-afeiship.sh;
 
 export NODE_PATH=$HOME/.node_modules;
 
+# modify default global path:
+export PATH=~/.npm-global/bin:$PATH;
+
 # brew install nodejs
 export PATH="/usr/local/opt/openssl/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/openssl/lib"

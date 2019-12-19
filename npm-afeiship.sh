bb4ac051a85f4f55a1d4721eb143d4072f2f13be
afeiship <1290657123@qq.com>
2019-12-19 20:29:04 +0800

    add: nf/nf-d/nf-s

--- npm-afeiship.sh
+++ npm-afeiship.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# my package from github
 alias na='_(){ npm install --registry=https://registry.npm.taobao.org; unset -f _; }; _';
 alias na-s='_(){ npm install afeiship/$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
 alias na-d='_(){ npm install afeiship/$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+
+# my package from nodejs
+alias nf='_(){ npm install --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias nf-s='_(){ npm install @feizheng/$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias nf-d='_(){ npm install @feizheng/$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';

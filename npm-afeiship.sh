5a386c2fde48018ec034cce6a0af37b8b160fdc7
afeiship <1290657123@qq.com>
2020-11-27 17:28:23 +0800

    fix: nj/njn

--- npm-afeiship.sh
+++ npm-afeiship.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # my package from github
-alias na='_(){ npm install --registry=https://registry.npm.taobao.org; unset -f _; }; _';
-alias na-s='_(){ npm install afeiship/$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
-alias na-d='_(){ npm install afeiship/$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias na-s='_(){ npm install @afeiship/$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias na-d='_(){ npm install @afeiship/$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';
 
 # my package from nodejs
-alias nf='_(){ npm install --registry=https://registry.npm.taobao.org; unset -f _; }; _';
-alias nf-s='_(){ npm install @feizheng/$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
-alias nf-d='_(){ npm install @feizheng/$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias nj-s='_(){ npm install @jswork/$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias nj-d='_(){ npm install @jswork/$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias njn-s='_(){ npm install @jswork/next-$1 -S --registry=https://registry.npm.taobao.org; unset -f _; }; _';
+alias njn-d='_(){ npm install @jswork/next-$1 -D --registry=https://registry.npm.taobao.org; unset -f _; }; _';

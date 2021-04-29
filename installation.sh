924e9adff6e2676781a32e5371c7b99ce8dc33eb
afeiship <1290657123@qq.com>
2021-04-29 21:20:40 +0800

    feat: install scripts

--- installation.sh
+++ installation.sh
@@ -4,3 +4,15 @@
 # https://blog.csdn.net/mouday/article/details/83866431
 brew uninstall --ignore-dependencies nodejs
 brew install nodejs
+
+
+# brew site
+# https://formulae.brew.sh/formula/node
+
+brew install node
+brew install node@14
+
+
+# node@14	14.16.1	Platform built on V8 to build network applications
+# node@12	12.22.1	Platform built on V8 to build network applications
+# node@10	10.24.1	Platform built on V8 to build network applications

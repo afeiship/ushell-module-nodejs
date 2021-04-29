79d4ca73d2cfe94d94d45acc69e2b705dd085238
afeiship <1290657123@qq.com>
2021-04-29 21:07:27 +0800

    feat: update node@14

--- index.sh
+++ index.sh
@@ -12,6 +12,14 @@ export NODE_PATH=$HOME/.node_modules;
 export PATH=$HOME/.npm-global/bin:$PATH;
 export PATH=$HOME/local/bin:$PATH;
 
+# brew install node@14
+# brew upgrade node@14
+# nodejs
+export PATH="/usr/local/opt/node@14/bin:$PATH";
+
+# For compilers to find node@14 you may need to set:
+export LDFLAGS="-L/usr/local/opt/node@14/lib";
+export CPPFLAGS="-I/usr/local/opt/node@14/include";
 # brew install nodejs
 # export PATH="/usr/local/opt/openssl/bin:$PATH"
 # export LDFLAGS="-L/usr/local/opt/openssl/lib"

183f4e40a36ddacc4c479e784021f0f8050aa2ae
afeiship <1290657123@qq.com>
2021-07-19 00:15:30 +0800

    feat: add pnpm

--- index.sh
+++ index.sh
@@ -4,6 +4,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 
 source $ROOT_PATH/npm.sh;
+source $ROOT_PATH/pnpm.sh;
 source $ROOT_PATH/npm-afeiship.sh;
 
 export NODE_PATH=$HOME/.node_modules;
---
+++ pnnpm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+alias pi-d='pnpm install -D';
+alias pi-s='pnpm install';

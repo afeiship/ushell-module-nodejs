c99039f3b3ce197abf5d7e3f2d00d22fa7bc83e1
afeiship <1290657123@qq.com>
2022-07-19 22:27:51 +0800

    feat: rm ntl

--- index.sh
+++ index.sh
@@ -6,7 +6,6 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 source $ROOT_PATH/modules/bun.sh;
 source $ROOT_PATH/modules/fnm.sh;
 source $ROOT_PATH/modules/npm.sh;
-source $ROOT_PATH/modules/ntl.sh;
 source $ROOT_PATH/modules/nvm.sh;
 source $ROOT_PATH/modules/plop.sh;
 # source $ROOT_PATH/modules/pnpm.sh;
--- modules/ntl.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(uname) == Darwin ]]; then
-  alias ntlg="ntl $HOME/github/mac-settings";
-fi
-
-

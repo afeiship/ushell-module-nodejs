9e5da0d6fc95001f6224d5289881863d2c2895a5
afeiship <1290657123@qq.com>
2022-12-04 20:56:20 +0800

    feat: upt pnpm

--- modules/pnpm.sh
+++ modules/pnpm.sh
@@ -4,3 +4,7 @@ alias pi='pnpm install';
 alias pi-s='pnpm install -S';
 alias pi-d='pnpm install -D';
 alias pi-g='pnpm add -g';
+
+
+export PNPM_HOME="$HOME/Library/pnpm";
+export PATH="$PNPM_HOME:$PATH";

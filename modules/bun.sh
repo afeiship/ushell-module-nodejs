e6a256f6d28fb03e0e264e2cbadc2f0f23e108cc
afeiship <1290657123@qq.com>
2023-02-27 13:29:30 +0800

    feat: upt bun

--- modules/bun.sh
+++ modules/bun.sh
@@ -2,3 +2,10 @@
 
 export BUN_INSTALL="$HOME/.bun";
 export PATH="$BUN_INSTALL/bin:$PATH";
+
+alias bns="bun run start";
+alias bnd="bun run dev";
+alias bn-i="bun install";
+alias bn-s="bun add";
+alias bn-d="bun add --development ";
+alias bn-g="bun add --global ";
